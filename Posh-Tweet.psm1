20f2ed04992bfc6e451d1a3aab2bbab1604a6c03
Carlos Perez <carlos_perez@darkoperator.com>
2014-02-28 21:04:56 -0400

    More timeline commands.

--- Posh-Tweet.psm1
+++ Posh-Tweet.psm1
@@ -1,9 +1,9 @@
 ﻿
 <#
 .Synopsis
-   Short description
+   Saves the API Tokens for the Twitter Application Profile the module will use.
 .DESCRIPTION
-   Long description
+   Saves the API Tokens for the Twitter Application Profile the module will use.
 .EXAMPLE
    Example of how to use this cmdlet
 .EXAMPLE
@@ -74,9 +74,11 @@ function Set-TweetToken
 
 <#
 .Synopsis
-   Short description
+   Connect to the Twitter service using previously saved Applicantion OAuth 
+   information.
 .DESCRIPTION
-   Long description
+   Connect to the Twitter service using previously saved Applicantion OAuth 
+   information.
 .EXAMPLE
    Example of how to use this cmdlet
 .EXAMPLE
@@ -118,9 +120,11 @@ function Connect-TweetService
 
 <#
 .Synopsis
-   Short description
+   Returns settings (including current trend, geo and sleep time information) 
+   for the authenticating user.
 .DESCRIPTION
-   Long description
+   Returns settings (including current trend, geo and sleep time information) 
+   for the authenticating user.
 .EXAMPLE
    Example of how to use this cmdlet
 .EXAMPLE
@@ -158,9 +162,13 @@ function Get-TweetAccountSetting
 
 <#
 .Synopsis
-   Short description
+   Returns the current rate limits for methods belonging to the specified 
+   resource families.
 .DESCRIPTION
-   Long description
+   Each 1.1 API resource belongs to a "resource family" which is indicated 
+   in its method documentation. You can typically determine a method's 
+   resource family from the first component of the path after the resource 
+   version.
 .EXAMPLE
    Example of how to use this cmdlet
 .EXAMPLE
@@ -198,18 +206,23 @@ function Get-TweetApplicationRateLimit
 
 <#
 .Synopsis
-   Short description
+   Updates the authenticating user's current status, also known as tweeting. 
 .DESCRIPTION
-   Returns a collection of the most recent Tweets and retweets posted by the 
-   authenticating user and the users they follow.
+   Updates the authenticating user's current status, also known as tweeting. 
 .EXAMPLE
-   Example of how to use this cmdlet
-.EXAMPLE
-   Another example of how to use this cmdlet
+   Sending a Tweet
+
+   PS C:\> Send-Tweet -Message "@digininja cant complain"
+
+
+    ID        : 439382933800255488
+    Text      : @digininja cant complain
+    CreatedAt : 2/28/2014 12:53:49 PM +00:00
+    Source    : <a href="http://github.com/darkoperator" rel="nofollow">Posh-Tweet</a>
 #>
 function Send-Tweet
 {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'Std')]
     [OutputType([psobject])]
     Param
     (
@@ -219,7 +232,31 @@ function Send-Tweet
                    Position=0)]
         [ValidateLength(1,140)]
         [string]
-        $Message
+        $Message,
+
+        # The ID of an existing status that the update is in reply to.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        [long]
+        $ReplyToID,
+
+        # The latitude of the location this tweet refers to. This parameter 
+        # will be ignored unless it is inside the range -90.0 to +90.0 (North 
+        # is positive) inclusive.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   ParameterSetName = 'Geo')]
+        [string]
+        $Latitude,
+
+        # The longitude of the location this tweet refers to. The valid ranges
+        # for longitude is -180.0 to +180.0 (East is positive) inclusive. 
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   ParameterSetName = 'Geo')]
+        [string]
+        $Longitude
     )
 
     Begin
@@ -232,10 +269,24 @@ function Send-Tweet
         {
             $TwClient = $Global:TweetInstance
         }
+
+        $TweetOps = New-Object HigLabo.Net.Twitter.UpdateStatusCommand 
     }
     Process
     {
-        $TweetResult = $TwClient.UpdateStatus($Message)
+        $TweetOps.Status = $Message
+        if ($ReplyToID)
+        {
+            $TweetOps.InReplyToStatusID = $ReplyToID
+        }
+
+        if ($PSCmdlet.ParameterSetName -eq 'Geo')
+        {
+            $TweetOps.Longitude = $Longitude
+            $TweetOps.Latitude = $Latitude
+        }
+
+        $TweetResult = $TwClient.UpdateStatus($TweetOps)
         $TweetResult.pstypenames.insert(0,'Tweet.SentMessage')
         $TweetResult
     }
@@ -247,9 +298,12 @@ function Send-Tweet
 
 <#
 .Synopsis
-   Short description
+   Returns a collection of the most recent Tweets and retweets posted by 
+   the authenticating user and the users they follow. 
 .DESCRIPTION
-   Long description
+   Returns a collection of the most recent Tweets and retweets posted by the 
+   authenticating user and the users they follow. The home timeline is central 
+   to how most users interact with the Twitter service. 
 .EXAMPLE
    Example of how to use this cmdlet
 .EXAMPLE
@@ -319,6 +373,314 @@ function Get-TweetTimeline
 
 
         $TimelineResult = $TwClient.GetHomeTimeline($TimelineOptions)
+        foreach($Message in $TimelineResult)
+        {
+            $Message.pstypenames.insert(0,'Tweet.Message')
+            $Message | Add-Member -NotePropertyName Name -NotePropertyValue $Message.User.Name
+            $Message | Add-Member -NotePropertyName ScreenName -NotePropertyValue $Message.User.ScreenName
+            $Message
+        }
+    }
+    End
+    {
+    }
+}
+
+
+<#
+.Synopsis
+   Returns the 20 most recent mentions (tweets containing a users's 
+   @screen_name) for the authenticating user.
+.DESCRIPTION
+   Returns the 20 most recent mentions (tweets containing a users's 
+   @screen_name) for the authenticating user. The timeline returned 
+   is the equivalent of the one seen when you view your mentions on 
+   twitter.com. 
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-TweetMentionTimeline
+{
+    [CmdletBinding()]
+    [OutputType([psobject])]
+    Param
+    (
+        # Specifies the number of records to retrieve. Must be less than or 
+        # equal to 200. Defaults to 20.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [int]
+        $Count,
+
+        # This parameter will prevent replies from appearing in the returned 
+        # timeline. 
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true)]
+        [switch]
+        $ExcludeReplies,
+
+        # Returns results with an ID greater than (that is, more recent than)
+        # the specified ID. There are limits to the number of Tweets which can
+        # be accessed. If the limit of Tweets has occured since the since_id,
+        # the since_id will be forced to the oldest ID available.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true)]
+        [int32]
+        $SinceID
+    )
+
+    Begin
+    {
+        if (!(Test-Path variable:Global:TweetInstance ))
+        {
+            throw "No connection present."
+        }
+        else
+        {
+            $TwClient = $Global:TweetInstance
+        }
+
+        # Set options for the query of messages
+        $TimelineOptions = new-object HigLabo.Net.Twitter.GetMentionsTimelineCommand
+    }
+    Process
+    {
+        if ($Count)
+        {
+            $TimelineOptions.Count = $Count
+        }
+
+        if ($ExcludeReplies)
+        {
+            $TimelineOptions.ExcludeReplies = 'true'
+        }
+
+        if ($SinceID)
+        {
+            $TimelineOptions.SinceID = $SinceID
+        }
+
+
+        $TimelineResult = $TwClient.GetMentionsTimeline($TimelineOptions)
+        foreach($Message in $TimelineResult)
+        {
+            $Message.pstypenames.insert(0,'Tweet.Message')
+            $Message | Add-Member -NotePropertyName Name -NotePropertyValue $Message.User.Name
+            $Message | Add-Member -NotePropertyName ScreenName -NotePropertyValue $Message.User.ScreenName
+            $Message
+        }
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Returns the most recent tweets authored by the authenticating user 
+   that have been retweeted by others. 
+.DESCRIPTION
+   Returns the most recent tweets authored by the authenticating user 
+   that have been retweeted by others. 
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-TweetRetweetTimeline
+{
+    [CmdletBinding()]
+    [OutputType([psobject])]
+    Param
+    (
+        # Specifies the number of records to retrieve. Must be less than or 
+        # equal to 200. Defaults to 20.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [int]
+        $Count,
+
+        # This parameter will prevent replies from appearing in the returned 
+        # timeline. 
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true)]
+        [switch]
+        $ExcludeReplies,
+
+        # Returns results with an ID greater than (that is, more recent than)
+        # the specified ID. There are limits to the number of Tweets which can
+        # be accessed. If the limit of Tweets has occured since the since_id,
+        # the since_id will be forced to the oldest ID available.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true)]
+        [int32]
+        $SinceID
+    )
+
+    Begin
+    {
+        if (!(Test-Path variable:Global:TweetInstance ))
+        {
+            throw "No connection present."
+        }
+        else
+        {
+            $TwClient = $Global:TweetInstance
+        }
+
+        # Set options for the query of messages
+        $TimelineOptions = new-object HigLabo.Net.Twitter.GetRetweetsOfMeTimelineCommand
+    }
+    Process
+    {
+        if ($Count)
+        {
+            $TimelineOptions.Count = $Count
+        }
+
+        if ($ExcludeReplies)
+        {
+            $TimelineOptions.ExcludeReplies = 'true'
+        }
+
+        if ($SinceID)
+        {
+            $TimelineOptions.SinceID = $SinceID
+        }
+
+
+        $TimelineResult = $TwClient.GetRetweetsOfMeTimeline($TimelineOptions)
+        foreach($Message in $TimelineResult)
+        {
+            $Message.pstypenames.insert(0,'Tweet.Message')
+            $Message | Add-Member -NotePropertyName Name -NotePropertyValue $Message.User.Name
+            $Message | Add-Member -NotePropertyName ScreenName -NotePropertyValue $Message.User.ScreenName
+            $Message
+        }
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Returns the most recent tweets authored by the authenticating user 
+   that have been retweeted by others. 
+.DESCRIPTION
+   Returns the most recent tweets authored by the authenticating user 
+   that have been retweeted by others. 
+.EXAMPLE
+   PS C:\ Get-TweetUserTimeline -ScreenName jsnover -Count 2 -IncludeRT
+
+
+    ID         : 437942603602862082
+    Name       : jsnover
+    ScreenName : jsnover
+    Text       : @SuchTechnology DSC is a platform that Chef/Puppet can layer on.  We'll get the ecosystem to write providers &amp; everyone can use them.
+    CreatedAt  : 2/24/2014 1:30:27 PM +00:00
+    Source     : web
+
+    ID         : 437941912608071680
+    Name       : jsnover
+    ScreenName : jsnover
+    Text       : RT @SuchTechnology: If you are your team's genius or you ever worked with one, please take 5 to read @jsnover post "On Heroes" http://t.co/…
+    CreatedAt  : 2/24/2014 1:27:43 PM +00:00
+    Source     : web
+
+#>
+function Get-TweetUserTimeline
+{
+    [CmdletBinding(DefaultParameterSetName = 'ScreenName')]
+    [OutputType([psobject])]
+    Param
+    (
+        # Specifies the number of records to retrieve. Must be less than or 
+        # equal to 200. Defaults to 20.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [int]
+        $Count,
+
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1,
+                   ParameterSetName = 'UserID')]
+        [string]
+        $UserID,
+
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1,
+                   ParameterSetName = 'ScreenName')]
+        [string]
+        $ScreenName,
+
+        # This parameter will prevent replies from appearing in the returned 
+        # timeline. 
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true)]
+        [switch]
+        $ExcludeReplies,
+
+        # Returns results with an ID greater than (that is, more recent than)
+        # the specified ID. There are limits to the number of Tweets which can
+        # be accessed. If the limit of Tweets has occured since the since_id,
+        # the since_id will be forced to the oldest ID available.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true)]
+        [int32]
+        $SinceID,
+
+        # Include ReTweets since the timeline will strip any native retweets 
+        # (though they will still count toward both the maximal length of the 
+        # timeline and the slice selected by the count parameter). 
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true)]
+        [switch]
+        $IncludeRT
+    )
+
+    Begin
+    {
+        if (!(Test-Path variable:Global:TweetInstance ))
+        {
+            throw "No connection present."
+        }
+        else
+        {
+            $TwClient = $Global:TweetInstance
+        }
+
+        # Set options for the query of messages
+        $TimelineOptions = new-object HigLabo.Net.Twitter.GetUserTimelineCommand
+    }
+    Process
+    {
+        if ($Count){ $TimelineOptions.Count = $Count }
+
+        if ($ExcludeReplies){ $TimelineOptions.ExcludeReplies = 'true' }
+
+        if ($SinceID){ $TimelineOptions.SinceID = $SinceID }
+
+        if ($IncludeRT){ $TimelineOptions.IncludeRts = "true" }
+
+        if ($PSCmdlet.ParameterSetName -eq 'ScreenName')
+        {
+            $TimelineOptions.ScreenName = $ScreenName
+        }
+        else
+        {
+            $TimelineOptions.UserID = $UserID
+        }
+
+
+        $TimelineResult = $TwClient.GetUserTimeline($TimelineOptions)
         foreach($Message in $TimelineResult)
         {
             $Message.pstypenames.insert(0,'Tweet.Message')
